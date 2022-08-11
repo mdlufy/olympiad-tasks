39728d068ed4577aeeb76c8d66c624516c9abaa7
mdlufy <german.panov.2018@mail.ru>
2022-08-11 21:52:53 +0300

    add double-sort anagrams solve

---
+++ training-contest-internship/E.py
@@ -0,0 +1,27 @@
+firstString = list(input())
+secondString = list(input())
+
+firstString.sort()
+secondString.sort()
+
+
+if firstString == secondString:
+    print(1)
+else:
+    print(0)
+
+
+# firstString = input()
+# secondString = input()
+
+# counter = 0
+
+# for symbol in secondString:
+# 	if symbol in firstString:
+# 		counter += 1
+
+# if counter == len(firstString):
+#     print(1)
+# else:
+#     print(0)
+
