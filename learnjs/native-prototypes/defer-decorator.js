fac74b6f64737bb754fdcda979b8e5e62a83f1ac
mdlufy <german.panov.2018@mail.ru>
2022-09-05 13:40:30 +0300

    daily learn js tasks solve

---
+++ learnjs/native-prototypes/defer-decorator.js
@@ -0,0 +1,14 @@
+Function.prototype.defer = function(ms) {
+    let func = this;
+
+    return function(...args) {
+        return setTimeout(() => func.apply(this, args), ms)
+    };
+
+}
+
+function f(a, b) {
+    console.log(a + b);
+}
+
+f.defer(1000)(1, 2); // выведет 3 через 1 секунду.
