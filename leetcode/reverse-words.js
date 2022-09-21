27b4f54d353e6505ecfd3874fc4102439ed61d11
mdlufy <german.panov.2018@mail.ru>
2022-09-21 14:11:38 +0300

    change reverse to unshift

--- leetcode/reverse-words.js
+++ leetcode/reverse-words.js
@@ -8,22 +8,22 @@ var reverseWords = function (s) {
 
     for (let i = s.length - 1; i >= 0; i--) {
         if (s[i] != " ") {
-            currWord.push(s[i]);
+            currWord.unshift(s[i]);
         } else {
             if (currWord.length !== 0) {
-                result.push(currWord.reverse().join(""));
+                result.push(currWord.join(""));
                 currWord = [];
             }
         }
     }
 
     if (currWord.length !== 0) {
-        result.push(currWord.reverse().join(""));
+        result.push(currWord.join(""));
     }
 
     return result.join(" ");
 };
 
-// console.log(reverseWords((s = "the sky is blue")));
+console.log(reverseWords((s = "the sky is blue")));
 console.log(reverseWords((s = "  hello world  ")));
-// console.log(reverseWords((s = "a good   example")));
+console.log(reverseWords((s = "a good   example")));
