9ee53bf4ed8d8f4cf904510aef2bf4a05a33fd21
mdlufy <german.panov.2018@mail.ru>
2022-09-29 14:20:28 +0300

    refactor solution

--- leetcode/isomorphic-strings.js
+++ leetcode/isomorphic-strings.js
@@ -3,30 +3,29 @@
  * @param {string} t
  * @return {boolean}
  */
-var isIsomorphic = function(s, t) {
+var isIsomorphic = function (s, t) {
     if (s.length !== t.length) return false;
 
-    const hashS = new Map();
-    const hashT = new Map();
+    const hash = new Map();
+    const set = new Set();
 
     for (let i = 0; i < s.length; i++) {
-        if (hashT.has(t[i])) {
-            if (s[i] !== hashT.get(t[i])) return false;
+        if (hash.has(s[i])) {
+            if (hash.get(s[i]) !== t[i]) return false;
         } else {
-            hashT.set(t[i], s[i]);
-        }
+            if (set.has(t[i])) {
+                return false;
+            }
 
-        if (hashS.has(s[i])) {
-            if (t[i] !== hashS.get(s[i])) return false;
-        } else {
-            hashS.set(s[i], t[i]);
+            hash.set(s[i], t[i]);
+            set.add(t[i]);
         }
     }
 
     return true;
 };
 
-console.log(isIsomorphic(s = "egg", t = "add"))
-console.log(isIsomorphic(s = "foo", t = "bar"))
-console.log(isIsomorphic('badc', 'baba'))
-console.log(isIsomorphic(s = "paper", t = "title"))
+console.log(isIsomorphic((s = 'egg'), (t = 'add')));
+console.log(isIsomorphic((s = 'foo'), (t = 'bar')));
+console.log(isIsomorphic('badc', 'baba'));
+console.log(isIsomorphic((s = 'paper'), (t = 'title')));
